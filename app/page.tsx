ed437002e7b2de6b4f837e9c0f38a1abed7d2063
Pham Anh Tien <anhtien13082005@gmail.com>
2025-01-14 21:33:52 +0700

    feat: update navbar

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        Locare
+        <h2 className="text-locare">Hello</h2>
+    </main>
+  );
+}
