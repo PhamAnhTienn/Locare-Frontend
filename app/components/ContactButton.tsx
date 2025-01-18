7e90130beb24e5deb0f5abd325c8ae3def56091e
Pham Anh Tien <anhtien13082005@gmail.com>
2025-01-18 22:12:34 +0700

    updates for property page

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,11 @@
+const ContactButton = () => {
+    return (
+        <div 
+            className="mt-6 py-4 px-6 cursor-pointer bg-locare text-white rounded-xl hover:bg-locare-dark transition"
+        >
+            Contact
+        </div>
+    );
+}
+
+export default ContactButton;
